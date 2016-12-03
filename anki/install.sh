bd6b4a3e0d68b69948065f9d88df573ea2a27224
Sam Seltzer-Johnston <sam@sbseltzer.net>
2016-12-03 16:15:02 -0500

    Updated install.sh to actually do something

--- anki/install.sh
+++ anki/install.sh
@@ -8,4 +8,10 @@ fi
 AnkiUser = "User 1";
 if [ ! -d "$AnkiDir/$AnkiUser" ]; then
     exit;
-fi;
+fi
+AnkiMedia = "$AnkiDir/$AnkiUser/collection.media"
+
+cp include.js "$AnkiMedia/_ja-srs-utils.include.js" 
+cp ../common/card.js "$AnkiMedia/_ja-srs-utils.card.js"
+cp ../common/card.css "$AnkiMedia/_ja-srs-utils.card.css"
+cp ../common/jquery-3.1.1.slim.min.js "$AnkiMedia/_ja-srs-utils.jquery-3.1.1.slim.min.js"
