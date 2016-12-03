88d05119b298eab81f8397685f9dfa61a03e878d
Sam <sam@sbseltzer.net>
2016-12-03 18:21:20 -0500

    Cleanup and added windows directory finding support

--- anki/install.sh
+++ anki/install.sh
@@ -1,17 +1,24 @@
-#!/bin/sh
+#!bash
 # Installation script for Anki
 
-AnkiDir = "~/Documents/Anki";
+AnkiDir="~/Documents/Anki";
 if [ ! -d $AnkiDir ]; then
-    exit;
+    TempDocumentsPath=`powershell -Command '[Environment]::GetFolderPath("MyDocuments")'`
+    TempDocumentsPath=${TempDocumentsPath/\\/\/}
+    AnkiDir=$TempDocumentsPath"/Anki"
 fi
-AnkiUser = "User 1";
+echo "Found Anki directory: $AnkiDir"
+if [ ! -d $AnkiDir ]; then
+    exit
+fi
+AnkiUser="User 1";
 if [ ! -d "$AnkiDir/$AnkiUser" ]; then
-    exit;
+    exit
 fi
-AnkiMedia = "$AnkiDir/$AnkiUser/collection.media"
-
-cp include.js "$AnkiMedia/_ja-srs-utils.include.js" 
-cp ../common/card.js "$AnkiMedia/_ja-srs-utils.card.js"
-cp ../common/card.css "$AnkiMedia/_ja-srs-utils.card.css"
-cp ../common/jquery-3.1.1.slim.min.js "$AnkiMedia/_ja-srs-utils.jquery-3.1.1.slim.min.js"
+echo "Found Anki user directory: $AnkiDir/$AnkiUser"
+AnkiMedia="$AnkiDir/$AnkiUser/collection.media"
+AnkiFilePrefix="$AnkiMedia/_ja-srs-utils."
+cp include.js "${AnkiFilePrefix}.include.js"
+cp ../common/card.js "${AnkiFilePrefix}.card.js"
+cp ../common/card.css "${AnkiFilePrefix}.card.css"
+cp ../common/jquery-3.1.1.slim.min.js "${AnkiFilePrefix}.jquery-3.1.1.slim.min.js"
