e5ce50003a94e5ae922453ec07fc657b210d07ba
Sam <sam@sbseltzer.net>
2016-12-04 14:57:51 -0500

    Added an install script for the example and updated include.js to reflect those changes

--- example/include.js
+++ example/include.js
@@ -1,15 +1,15 @@
 console.log("Including scripts")
 var jqScript = document.createElement("script");
 jqScript.type = "text/javascript";
-jqScript.src = "../common/jquery-3.1.1.slim.min.js";
+jqScript.src = "jquery-3.1.1.slim.min.js";
 function loadCardDependencies() {
     var cardScript = document.createElement("script");
     cardScript.type = "text/javascript";
-    cardScript.src = "../common/card.js";
+    cardScript.src = "card.js";
     document.body.appendChild(cardScript);
     var cardStyle = document.createElement("link");
     cardStyle.rel = "stylesheet";
-    cardStyle.href = "../common/card.css";
+    cardStyle.href = "card.css";
     document.body.appendChild(cardStyle);
 }
 jqScript.onreadystatechange= function () {
---
+++ example/install.sh
@@ -0,0 +1,7 @@
+#!bash
+# Example installation script
+
+cp ../common/card.js .
+cp ../common/card.css .
+cp ../common/StrokeOrder.ttf .
+cp ../common/jquery-3.1.1.slim.min.js .
