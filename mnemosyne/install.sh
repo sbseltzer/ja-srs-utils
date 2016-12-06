ae0734dc5d948cd54568a3140fef6adb60289ce4
Sam Seltzer-Johnston <samuel.seltzer-johnston@movimentogroup.com>
2016-12-06 15:55:26 -0500

    Made install script combine all into one file (hackishly) - seems to work, but fonts aren't working

--- mnemosyne/install.sh
+++ mnemosyne/install.sh
@@ -5,11 +5,6 @@ cp ../common/card.js .
 cp ../common/card.css .
 cp ../common/StrokeOrder.ttf .
 cp ../common/jquery-3.1.1.slim.min.js .
-
-cp ../common/include.js .
-sed -i -e "s/JQUERY/${namePrefix}jquery-3.1.1.slim.min.js/g" include.js
-sed -i -e "s/CARD_JS/${namePrefix}card.js/g" include.js
-sed -i -e "s/CARD_CSS/${namePrefix}card.css/g" include.js
 sed -i -e "s/StrokeOrder.ttf/${namePrefix}StrokeOrder.ttf/g" card.css
 
 MnemosyneDir="$APPDATA/Mnemosyne/default.db_media"
@@ -17,9 +12,8 @@ if [ ! -d "$MnemosyneDir" ]; then
     echo "Failed to find Mnemosyne media directory!"
     exit
 fi
-
+echo "document.write(\`<style>$(cat card.css)</style>\`);" > card.css.js;
+cat jquery-3.1.1.slim.min.js card.js card.css.js > include.js
 mv {,"$MnemosyneDir/$namePrefix"}"include.js"
-mv {,"$MnemosyneDir/$namePrefix"}"card.js"
-mv {,"$MnemosyneDir/$namePrefix"}"card.css"
 mv {,"$MnemosyneDir/$namePrefix"}"StrokeOrder.ttf"
-mv {,"$MnemosyneDir/$namePrefix"}"jquery-3.1.1.slim.min.js"
+ 
