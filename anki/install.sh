8089f70631dca408b191e1842988c7892b9a533d
Sam <sam@sbseltzer.net>
2016-12-05 20:13:28 -0500

    Cleaned up install file while fixing issues on Windows

--- anki/install.sh
+++ anki/install.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 # Installation script for Anki
-UserDocs=`cat ~/.config/user-dirs.dirs | grep DOCUMENTS`
 # Try to find Documents dir on Linux
-eval $UserDocs
-UserDocs=$XDG_DOCUMENTS_DIR
-# Fallback for Linux
-if [ ! -d $AnkiDir ]; then
-    $UserDocs="~/Documents/Anki"
+if [ -f "~/.config/user-dirs.dirs" ]; then
+    echo "Attempting to get Documents directory using ~/.config/user-dirs.dirs"
+    UserDocs=`cat ~/.config/user-dirs.dirs | grep DOCUMENTS`
+    eval $UserDocs
+    UserDocs=$XDG_DOCUMENTS_DIR
+    # Fallback for Linux
+    if [ ! -d $UserDocs ]; then
+        $UserDocs="~/Documents/Anki"
+    fi
 fi
-# Fallback for Windows
-if [ ! -d $AnkiDir ]; then
+# Try to find Documents dir on Windows
+if [ ! -d "$UserDocs" ]; then
+    echo "Attempting to get Documents directory using powershell"
     UserDocs=`powershell -Command '[Environment]::GetFolderPath("MyDocuments")'`
-    UserDocs=${TempDocumentsPath/\\/\/}
+    UserDocs=${UserDocs/\\/\/}
 fi
-AnkiUser=${1:="User 1"};
+
 AnkiDir="$UserDocs/Anki";
-if [ ! -d $AnkiDir ]; then
+if [ ! -d "$AnkiDir" ]; then
     exit
 fi
 echo "Found Anki directory: $AnkiDir"
+
+# Find Anki user directory
+AnkiUser=${1:="User 1"};
 if [ ! -d "$AnkiDir/$AnkiUser" ]; then
     exit
 fi
 echo "Found Anki user directory: $AnkiDir/$AnkiUser"
+
 AnkiMedia="$AnkiDir/$AnkiUser/collection.media"
 AnkiNamePrefix="_ja-srs-utils";
 AnkiFilePrefix="$AnkiMedia/$AnkiNamePrefix"
