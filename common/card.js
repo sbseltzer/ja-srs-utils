0f1d9b47d9ee0cd2dc1d99eedb0d423fb4284e7f
Sam <sam@sbseltzer.net>
2016-12-07 00:12:44 -0500

    In the process of rewriting furigana parser

--- common/card.js
+++ common/card.js
@@ -1,104 +1,164 @@
 function isInUnicodeRange(code, start, end) {
     return start <= code && code <= end;
 }
-function isPunctuation(str, atIndex) {
-    var c = str.charCodeAt(atIndex);
+function isPunctuation(text, atIndex) {
+    var c = text.charCodeAt(atIndex);
     return isInUnicodeRange(c, 0x3000, 0x303f);
 }
-function isHiragana(str, atIndex) {
-    var c = str.charCodeAt(atIndex);
+function isHiragana(text, atIndex) {
+    var c = text.charCodeAt(atIndex);
     return isInUnicodeRange(c, 0x3040, 0x309f);
 }
-function isKatakana(str, atIndex) {
-    var c = str.charCodeAt(atIndex);
+function isKatakana(text, atIndex) {
+    var c = text.charCodeAt(atIndex);
     return isInUnicodeRange(c, 0x30a0, 0x30ff);
 }
-function isFWRomanOrHWKatakana(str, atIndex) {
-    var c = str.charCodeAt(atIndex);
+function isFWRomanOrHWKatakana(text, atIndex) {
+    var c = text.charCodeAt(atIndex);
     return isInUnicodeRange(c, 0xff00, 0xffef);
 }
-function isKanji(str, atIndex) {
-    var c = str.charCodeAt(atIndex);
+function isKanji(text, atIndex) {
+    var c = text.charCodeAt(atIndex);
     return isInUnicodeRange(c, 0x4e00, 0x9faf);
 }
 
-function getStartIndexOfKanjiAt(str, atIndex) {
-    if  (!isKanji(str, atIndex)) {
+function getStartIndexOfKanjiAt(text, atIndex) {
+    if  (!isKanji(text, atIndex)) {
         return -1;
     }
     while (atIndex > 0) {
-        if (!isKanji(str, atIndex)) {
+        if (!isKanji(text, atIndex)) {
             return atIndex + 1;
         }
         atIndex--;
     }
     return 0;
 }
-function getEndIndexOfKanjiAt(str, atIndex) {
-    if  (!isKanji(str, atIndex)) {
+function getEndIndexOfKanjiAt(text, atIndex) {
+    if  (!isKanji(text, atIndex)) {
         return -1;
     }
-    while (atIndex < str.length) {
-        if (!isKanji(str, atIndex)) {
+    while (atIndex < text.length) {
+        if (!isKanji(text, atIndex)) {
             return atIndex - 1;
         }
         atIndex++;
     }
-    return str.length - 1;
+    return text.length - 1;
 }
 
-function getIndexOfNextOpeningBrace(str, atIndex, braceType) {
-    return str.indexOf(braceType[0], atIndex);
+function getIndexOfNextOpeningBrace(text, atIndex, braceType) {
+    return text.indexOf(braceType[0], atIndex);
 }
-function getIndexOfNextClosingBrace(str, atIndex, braceType) {
-    return str.indexOf(braceType[1], atIndex);
+function getIndexOfNextClosingBrace(text, atIndex, braceType) {
+    return text.indexOf(braceType[1], atIndex);
 }
 
-function parseTextWithFuri(container) {
-    var str = container.text();
-    braceType = container.attr("brace-type");
-    braceType = (braceType != undefined) ? braceType : "()";
-    console.log(str);
-    container.html("");
-    var elements = container;
-    var searchText = "";
+function makeRuby(partA, partB, braceType) {
+    var ruby = $("<ruby></ruby>");
+    ruby.append(partA);
+    ruby.append("<rp>" + braceType[0] + "</rp>");
+    ruby.append(partB);
+    ruby.append("<rp>" + braceType[1] + "</rp>");
+    return ruby;
+}
+function parseTextNodeWithFurigana(textNode, braceType) {
+    var elements = $("<span></span>");
+    var text = textNode.text();
+    console.log(braceType);
+    // if (text.startsWith(braceType[0])) {
+    //     // Separate the first markup out to pair with the previous ruby.
+    //     var start = getIndexOfNextOpeningBrace(text, 0, braceType);
+    //     var end = getIndexOfNextClosingBrace(text, start + 1, braceType);
+    //     var furi = text.substring(start + 1, end);
+    //     var ruby = makeRuby(textNode.prev(), furi);
+    //     text.text(text.substring(end + 1));
+    //     elements.append(ruby);
+    //     elements.append(text);
+    // }
+    // text = text.text();
     var index = 0;
-    var endIndex = str.length;
+    var endIndex = text.length;
     while (index < endIndex) {
-        var bracesStart = getIndexOfNextOpeningBrace(str, index, braceType);
+        var bracesStart = getIndexOfNextOpeningBrace(text, index, braceType);
         console.log("index:", index, "bracesStart:", bracesStart);
         if (bracesStart == -1) {
-            var remainder = str.substring(index);
+            var remainder = text.substring(index);
             elements.append(remainder);
-            searchText += remainder;
             break;
         }
-
-        var bracesEnd = getIndexOfNextClosingBrace(str, bracesStart, braceType);
-        var kanjiStart = getStartIndexOfKanjiAt(str, bracesStart - 1);
+        var bracesEnd = getIndexOfNextClosingBrace(text, bracesStart, braceType);
+        var kanjiStart = bracesStart - 1;
         var kanjiEnd = bracesStart;
         console.log("bracesEnd:", bracesEnd, "kanjiStart:", kanjiStart, "kanjiEnd:", kanjiEnd);
         index = bracesEnd + 1;
-
-        var kanjiElement = $("<rb>" + str.substring(kanjiStart, kanjiEnd) + "</rb>");
-        searchText += kanjiElement.text();
-        var furiElement = $("<rt>" + str.substring(bracesStart + 1, bracesEnd) + "</rt>");
-        var element = $("<ruby></ruby>");
-        element.append(kanjiElement);
-        element.append("<rp>(</rp>");
-        element.append(furiElement);
-        element.append("<rp>)</rp>");
-        elements.append(element);
+        elements.append(makeRuby(text.substring(kanjiStart, kanjiEnd), text.substring(bracesStart + 1, bracesEnd), braceType));
     }
-    return searchText;
+    textNode.replaceWith(elements);
+    return elements;
 }
+
+function parseFuriganaMarkup(container) {
+    container.find("*").each(function() {
+        var e = $(this);
+        var braceType = container.attr("brace-type");
+        braceType = (braceType != undefined) ? braceType : "()";
+        console.log(braceType);
+        if (e.nodeType == 3) {
+            // Text nodes
+        var elements = parseTextNodeWithFurigana(e, braceType);
+            e.html(elements);
+        }
+    });
+}
+
+// function parseTextWithFuri(container) {
+//     var text = container.text();
+//     braceType = container.attr("brace-type");
+//     braceType = (braceType != undefined) ? braceType : "()";
+//     console.log(text);
+//     container.html("");
+//     var elements = container;
+//     var searchText = "";
+//     var index = 0;
+//     var endIndex = text.length;
+//     while (index < endIndex) {
+//         var bracesStart = getIndexOfNextOpeningBrace(text, index, braceType);
+//         console.log("index:", index, "bracesStart:", bracesStart);
+//         if (bracesStart == -1) {
+//             var remainder = text.substring(index);
+//             elements.append(remainder);
+//             searchText += remainder;
+//             break;
+//         }
+
+        // var bracesEnd = getIndexOfNextClosingBrace(text, bracesStart, braceType);
+        // var kanjiStart = getStartIndexOfKanjiAt(text, bracesStart - 1);
+        // var kanjiEnd = bracesStart;
+        // console.log("bracesEnd:", bracesEnd, "kanjiStart:", kanjiStart, "kanjiEnd:", kanjiEnd);
+        // index = bracesEnd + 1;
+
+//         var kanjiElement = $("<rb>" + text.substring(kanjiStart, kanjiEnd) + "</rb>");
+//         searchText += kanjiElement.text();
+//         var furiElement = $("<rt>" + text.substring(bracesStart + 1, bracesEnd) + "</rt>");
+//         var ruby = $("<ruby></ruby>");
+//         ruby.append(kanjiElement);
+//         ruby.append("<rp>(</rp>");
+//         ruby.append(furiElement);
+//         ruby.append("<rp>)</rp>");
+//         elements.append(ruby);
+//     }
+//     return searchText;
+// }
 
 $("[use-furigana]").each(
     function() {
         var e = $(this);
         console.log(e.text());
-        var searchText = parseTextWithFuri(e);
-        e.attr("search", searchText);
+        parseFuriganaMarkup(e);
+        // var searchText = parseTextWithFuri(e);
+        // e.attr("search", searchText);
+        e.attr("search", e.text());
     });
 
 function cycleFont(e, fontlist)
